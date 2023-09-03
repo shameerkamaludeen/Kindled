0581e9ee536fc9737c7c56f3842db8c9d70b2331
Shameer Kamaludeen <shameer.plsn@gmail.com>
2023-09-03 06:42:49 +0530

    complete mobile view first review

---
+++ scss/blocks/brand/brands-img-swiper/brands-img-swiper.js
@@ -0,0 +1,22 @@
+/* || Bloack - Brands img swiper
+   ========================================================================== */
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
+
+export default function addBrandsISEvents() {
+  const swiper = new Swiper('.brands-is-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 2,
+    breakpoints: {
+      576: {
+        slidesPerView: 3,
+      },
+      992: {
+        slidesPerView: 4,
+      },
+      1024: {
+        slidesPerView: 5,
+      },
+    }
+  })
+}
